fa210ab76fc1fd0f193fa542018cf6057cee9392
Vu Duong <vuduong@vus-macbook-pro.lan>
2019-10-09 23:21:21 -0400

    added password match feature

--- Artable/Controller/RegisterVC.swift
+++ Artable/Controller/RegisterVC.swift
@@ -15,20 +15,51 @@ class RegisterVC: UIViewController {
     @IBOutlet weak var emailTxt: UITextField!
     @IBOutlet weak var passwordTxt: UITextField!
     @IBOutlet weak var confirmPassTxt: UITextField!
-    
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var passCheckImg: UIImageView!
+    @IBOutlet weak var confirmPassCheckImg: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        passwordTxt.addTarget(self, action: #selector(textFieldDidChange(_:)), for: UIControl.Event.editingChanged)
         
+        confirmPassTxt.addTarget(self, action: #selector(textFieldDidChange(_:)), for: UIControl.Event.editingChanged)
+        
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        
+        guard let passText = passwordTxt.text else { return }
+        
+        if textField == confirmPassTxt {
+            passCheckImg.isHidden = false
+            confirmPassCheckImg.isHidden = false
+        } else {
+            if passText.isEmpty {
+                passCheckImg.isHidden = true
+                confirmPassCheckImg.isHidden = true
+                confirmPassTxt.text = ""
+            }
+        }
+        
+        if passwordTxt.text == confirmPassTxt.text {
+            passCheckImg.image = UIImage(named: "green_check")
+            confirmPassCheckImg.image = UIImage(named: "green_check")
+        } else {
+            passCheckImg.image = UIImage(named: "red_check")
+            confirmPassCheckImg.image = UIImage(named: "red_check")
+        }
     }
     
     @IBAction func registerClicked(_ sender: Any) {
         
-        guard let email = emailTxt.text , !email.isEmpty ,
-            let username = usernameTxt.text, !username.isEmpty ,
-            let password = passwordTxt.text, !password.isEmpty else { return }
+        guard let email = emailTxt.text , email.isNotEmpty ,
+            let username = usernameTxt.text, username.isNotEmpty ,
+            let password = passwordTxt.text, password.isNotEmpty else { return }
+        
+        activityIndicator.startAnimating()
         
         Auth.auth().createUser(withEmail: email, password: password) { (authResults, error) in
             
@@ -38,6 +69,9 @@ class RegisterVC: UIViewController {
             }
             
             guard let user = authResults?.user else { return }
+            
+            self.activityIndicator.stopAnimating()
+            
             print("\(user.email!) created")
         }
     }
---
+++ Shared/Utils/Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Extensions.swift
+//  Artable
+//
+//  Created by Vu Duong on 10/9/19.
+//  Copyright © 2019 Vu Duong. All rights reserved.
+//
+
+import Foundation
+
+
+extension String {
+    
+    var isNotEmpty : Bool {
+        return !isEmpty
+    }
+    
+}
